20b166717c58e81b5556e378cd07145c967aa8ed
vtlai88 <vtlai@ucdavis.edu>
2020-07-09 00:11:20 -0700

    first commit

---
+++ src/Dashboard/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+//links to the Dashboard page
+
+function Dashboard() {
+    return (
+      <div>
+        <h2>Dashboard</h2>
+      </div>
+    );
+  }
+
+  export default Dashboard;
